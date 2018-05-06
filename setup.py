dfe224aeac433a7c77f08aa313744eac4b0374c8
Konstantin Maslyuk <kostyamaslyuk@gmail.com>
2018-05-06 21:41:24 +0300

    Add two distutils files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='calldict',
+  packages=['calldict'],
+  version='0.1',
+  description='Call functions stored in a dictionaries',
+  author='Konstantin Maslyuk',
+  author_email='kostyamaslyuk@gmail.com',
+  url='https://github.com/kalemas/calldict',
+  download_url='https://github.com/kalemas/calldict/archive/master.zip',
+  keywords=['dict', 'evaluation', 'yaml'],
+  classifiers=[],
+)
